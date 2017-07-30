239f00518233cb2c01cc586eb2304998d1cc09e1
ffon <poty1900@hotmail.com>
2017-07-30 15:02:22 +0700

    Update test_pushMsg.php

--- test_pushMsg.php
+++ test_pushMsg.php
@@ -1,9 +1,9 @@
 <?php
-$token = $_GET['token'];
+$id = $_GET['id'];
 $mid = $_GET['mid'];
 $text = $_GET['text'];
 
-var_dump($token);echo "<br>";
+var_dump($id);echo "<br>";
 var_dump($mid);echo "<br>";
 var_dump($text);echo "<br>";
 
