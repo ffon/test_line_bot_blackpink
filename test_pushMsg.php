b4746a4dc71eb1c707eeb34364aa79c5d862eafe
ffon <poty1900@hotmail.com>
2017-07-31 12:42:59 +0700

    Update test_pushMsg.php

--- test_pushMsg.php
+++ test_pushMsg.php
@@ -7,15 +7,19 @@ function getToken()
     $id = $_GET['id'];
     $mid = $_GET['mid'];
     $text = $_GET['text'];
+
     echo "id";echo "<br>";
     var_dump($id);
+
     echo "<br>";
     echo "mid";echo "<br>";
     var_dump($mid);
+
     echo "<br>";
     echo "Text";echo "<br>";
     var_dump($text);
-    echo "<br>";
+
+    
     
     
     $chAdd = curl_init();
@@ -29,17 +33,21 @@ function getToken()
     $result = curl_exec($chAdd);
     $err    = curl_error($chAdd);
     curl_close($chAdd);
+
     $line_master = json_decode($result);
     $count = count($line_master);
     
-    $j=0;
-    while ($j!=$count) {
-        if ($id==$line_master[$j]->id) {
-            $token=$line_master[$j]->access_token;
-        }
-    }
-    echo "token";echo "<br>";
-    echo $token;
+    echo "<br>";
+    var_dump($line_master);
+    // $j=0;
+    // while ($j!=$count) {
+    //     // if ($id==$line_master[$j]->id) {
+    //         $token=$line_master[$j]->access_token;
+    //     //}
+    // }
+    // echo "<br>";
+    // echo "token";echo "<br>";
+    // echo $token;
 }
 
 
