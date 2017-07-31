a410d4788a3426dbee0548ada656675077d7744d
ffon <poty1900@hotmail.com>
2017-08-01 00:00:25 +0700

    Update Muti_test_member.php

--- Muti_test_member.php
+++ Muti_test_member.php
@@ -8,4 +8,20 @@ var_dump($id);
 echo "access_token";echo "<br>";
 var_dump($access_token);
 
+$chAdd = curl_init();
+curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/get_line_member');
+curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($chAdd, CURLOPT_HTTPHEADER, array(
+"Content-Type: application/json",
+                    )
+);
+$result = curl_exec($chAdd);
+$err    = curl_error($chAdd);
+curl_close($chAdd);
+
+$line_member = json_decode($result);
+$count = count($line_member);
+
+
 ?>
