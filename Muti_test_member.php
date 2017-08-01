9893d3ef38bb4c51fe046efdce5c4de210c62972
ffon <poty1900@hotmail.com>
2017-08-01 10:15:45 +0700

    Update Muti_test_member.php

--- Muti_test_member.php
+++ Muti_test_member.php
@@ -1,20 +1,17 @@
 <?php
 
 $id = $_GET['id'];
-$access_token = $_GET['access_token'];
-$line_name = $_GET['line_name'];
+// $access_token = $_GET['access_token'];
+// $line_name = $_GET['line_name'];
 
 echo "id";echo "<br>";
 var_dump($id);echo "<br>";
 
-echo "access_token";echo "<br>";
-var_dump($access_token);echo "<br>";
+// echo "access_token";echo "<br>";
+// var_dump($access_token);echo "<br>";
 
-echo "line_name";echo "<br>";
-var_dump($line_name);echo "<br>";
-
-
-
+// echo "line_name";echo "<br>";
+// var_dump($line_name);echo "<br>";
 
 
 ?>
