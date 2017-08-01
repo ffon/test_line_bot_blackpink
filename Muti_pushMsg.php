75685c50bcb10e50262e94ba5d42683b0be81e45
ffon <poty1900@hotmail.com>
2017-08-01 14:35:58 +0700

    Update Muti_pushMsg.php

--- Muti_pushMsg.php
+++ Muti_pushMsg.php
@@ -1,93 +1,47 @@
 <?php
-get_token();
 
-function get_token()
-{    
-    $id = $_GET['token'];
+    $token = $_GET['token'];
     $mid = $_GET['mid'];
     $text = $_GET['text'];
     
-    echo "id"; echo "<br>";
-    var_dump($id); echo "<br>";
-    echo "mid"; echo "<br>";
-    var_dump($mid); echo "<br>";
-    echo "text"; echo "<br>";
-    var_dump($text); echo "<br>";
-
-//     $ch = curl_init();
-//     curl_setopt($ch, CURLOPT_URL, 'https://uat.dxplace.com/dxtms/get_line_master');
-//     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//     "Content-Type: application/json",
-//                             )
-//     );
-//     $result = curl_exec($ch);
-//     $err    = curl_error($ch);
-//     curl_close($ch);
-//     $line_master = json_decode($result);
-//     $count = count($line_master);
-//     var_dump($id);
-//     echo "<br>";
-    
-//     $count_id=count($id);
-//     echo "count_id";
-//     echo "<br>";
-//     var_dump($count_id);
-//     echo "<br>";
-    
-//     for ($i=0; $i<$count; $i++) {
-//         for ($j=0; $j<$count_id; $j++) {
-//             if ($id[$j]==$line_master[$i]->id) {
-//                         $token[$j] = $line_master[$i]->access_token;
-//             }
-//         }
-//     }
-   
-//     echo "token";
-//     echo "<br>";
-//     var_dump($token);
-//     echo "<br>";
-
-    //pushMsg($token,$mid,$text);
-}
-
-function pushMsg($token,$mids,$text){
-   
-    echo "mid";echo "<br>";
-    var_dump($mids);echo "<br>";
-    echo "Text";echo "<br>";
+    echo "id";
+    echo "<br>";
+    var_dump($id);
+    echo "<br>";
+    echo "mid";
+    echo "<br>";
+    var_dump($mid);
+    echo "<br>";
+    echo "text";
+    echo "<br>";
     var_dump($text);
-    echo "token";echo "<br>";
-    echo $token;echo "<br>";
+    echo "<br>";
     
-    
-    foreach($mids as $key => $mid){        
-        $messages = [
-            "type" => "text",
-            "text" => $text
-        ];
+foreach ($mids as $key => $mid) {
+    $messages = [
+    "type" => "text",
+    "text" => $text
+    ];
  
-        $post_data = [
-            "to" => $mid,
-            "messages" => [$messages]
-        ];
+    $post_data = [
+    "to" => $mid,
+    "messages" => [$messages]
+    ];
  
-        $header = array(
-            'Content-Type: application/json',
-            'Authorization: Bearer ' . $token
-        );
-        $url = 'https://api.line.me/v2/bot/message/push';
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        $result = curl_exec($ch);
-        curl_close($ch);
+    $header = array(
+    'Content-Type: application/json',
+    'Authorization: Bearer ' . $token
+    );
+    $url = 'https://api.line.me/v2/bot/message/push';
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    $result = curl_exec($ch);
+    curl_close($ch);
 }
-//         echo "result ";echo "<br>";
-//         var_dump($result);
-}
-?>
+    echo "result ";
+    echo "<br>";
+    var_dump($result);
