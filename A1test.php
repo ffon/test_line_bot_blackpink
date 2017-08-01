61273d858dd5ee5d0f89a1baa3999d7d07cf65cd
ffon <poty1900@hotmail.com>
2017-08-01 13:02:03 +0700

    Create A1test.php

---
+++ A1test.php
@@ -0,0 +1,18 @@
+<?php
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/get_line_master');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+"Content-Type: application/json",
+                        )
+);
+$result = curl_exec($ch);
+$err    = curl_error($ch);
+curl_close($ch);
+
+
+var_dump($result);
+
+?>
