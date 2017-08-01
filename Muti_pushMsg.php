7718aacc0939ba3b6adaca987e078a9f3e510aa6
ffon <poty1900@hotmail.com>
2017-08-01 14:11:02 +0700

    Update Muti_pushMsg.php

--- Muti_pushMsg.php
+++ Muti_pushMsg.php
@@ -1,45 +1,53 @@
 <?php
 get_token();
+
 function get_token()
 {    
     $id = $_GET['id'];
     $mid = $_GET['mid'];
     $text = $_GET['text'];
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://uat.dxplace.com/dxtms/get_line_master');
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    "Content-Type: application/json",
-                            )
-    );
-    $result = curl_exec($ch);
-    $err    = curl_error($ch);
-    curl_close($ch);
-    $line_master = json_decode($result);
-    $count = count($line_master);
+    
+    echo "id"; echo "<br>";
     var_dump($id);
-    echo "<br>";
+    echo "mid"; echo "<br>";
+    var_dump($mid);
+    echo "text"; echo "<br>";
+    var_dump($text);
+
+//     $ch = curl_init();
+//     curl_setopt($ch, CURLOPT_URL, 'https://uat.dxplace.com/dxtms/get_line_master');
+//     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//     "Content-Type: application/json",
+//                             )
+//     );
+//     $result = curl_exec($ch);
+//     $err    = curl_error($ch);
+//     curl_close($ch);
+//     $line_master = json_decode($result);
+//     $count = count($line_master);
+//     var_dump($id);
+//     echo "<br>";
     
-    $count_id=count($id);
-    echo "count_id";
-    echo "<br>";
-    var_dump($count_id);
-    echo "<br>";
+//     $count_id=count($id);
+//     echo "count_id";
+//     echo "<br>";
+//     var_dump($count_id);
+//     echo "<br>";
     
-    for ($i=0; $i<$count; $i++) {
-        for ($j=0; $j<$count_id; $j++) {
-            if ($id[$j]==$line_master[$i]->id) {
-                        $token[$j] = $line_master[$i]->access_token;
-            }
-        }
-    }
+//     for ($i=0; $i<$count; $i++) {
+//         for ($j=0; $j<$count_id; $j++) {
+//             if ($id[$j]==$line_master[$i]->id) {
+//                         $token[$j] = $line_master[$i]->access_token;
+//             }
+//         }
+//     }
    
-    echo "token";
-    echo "<br>";
-    var_dump($token);
-    echo "<br>";
+//     echo "token";
+//     echo "<br>";
+//     var_dump($token);
+//     echo "<br>";
 
     //pushMsg($token,$mid,$text);
 }
@@ -79,7 +87,7 @@ function pushMsg($token,$mids,$text){
         $result = curl_exec($ch);
         curl_close($ch);
 }
-        echo "result ";echo "<br>";
-        var_dump($result);
+//         echo "result ";echo "<br>";
+//         var_dump($result);
 }
 ?>
