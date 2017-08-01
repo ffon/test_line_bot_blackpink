48164f5b8df721b02685023f0731d6d3dbff4e27
ffon <poty1900@hotmail.com>
2017-08-01 10:25:00 +0700

    Update Muti_test_member.php

--- Muti_test_member.php
+++ Muti_test_member.php
@@ -1,17 +1,43 @@
 <?php
-
 $id = $_GET['id'];
-// $access_token = $_GET['access_token'];
-// $line_name = $_GET['line_name'];
+
 
 echo "id";echo "<br>";
 var_dump($id);echo "<br>";
 
-// echo "access_token";echo "<br>";
-// var_dump($access_token);echo "<br>";
+get_token($id);
 
-// echo "line_name";echo "<br>";
-// var_dump($line_name);echo "<br>";
+function get_token($id){
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,'http://uat.dxplace.com/dxtms/get_line_master');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json",
+                            )
+    );
+    $result = curl_exec($ch);
+    $err    = curl_error($ch);
+    curl_close($ch);
+
+    $line_master = json_decode($result);
+    $count = count($line_master);
+
+    $i=0;
+    while($i!=$count){
+        if($line_master[$i]->id == $id){
+            $token = $line_master[$i]->access_token;
+        }
+        $i++;
+    }
+
+    echo "Token";echo "<br>";
+    var_dump($token);
+
+
+
+}
 
 
 ?>
