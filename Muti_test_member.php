47efc0bdb69e5f98775494e30033f0ac2b530eb8
ffon <poty1900@hotmail.com>
2017-08-03 10:27:43 +0700

    Update Muti_test_member.php

--- Muti_test_member.php
+++ Muti_test_member.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="th">
-111212
+1010
 <br>
 <head>
     <title>Push Messages</title>
@@ -74,12 +74,12 @@
                                     if ($id[$j] == $line_member[$i]->line_master_id) {
                                         //$mid[$id[$j]] = array($j => array($line_member[$i]->user_id));
                                         //$mid[$j] = array($id => array($line_member[$i]->user_id));
-                                        $mid[$count_id] = array($id => array($j => $line_member[$i]->user_id));
+                                        $mid[$j] = array($id => array($j => $line_member[$i]->user_id));
 
                                     ?>
 
                                         <div class="checkbox">
-                                                <label><input type="checkbox" value="<?php print_r($mid[$count_id]); ?>" name="mid[]"> 
+                                                <label><input type="checkbox" value="<?php print_r($mid[$j]); ?>" name="mid[]"> 
                                                 <?php
                                                     echo "(";
                                                     echo $line_member[$i]->created_user;
