cfef0aa97931317e3b943a34a2d67ae34ac8ac69
ffon <poty1900@hotmail.com>
2017-08-03 10:30:52 +0700

    Update Muti_test_member.php

--- Muti_test_member.php
+++ Muti_test_member.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="th">
-1111
+121213
 <br>
 <head>
     <title>Push Messages</title>
@@ -74,7 +74,7 @@
                                     if ($id[$j] == $line_member[$i]->line_master_id) {
                                         //$mid[$id[$j]] = array($j => array($line_member[$i]->user_id));
                                         //$mid[$j] = array($id => array($line_member[$i]->user_id));
-                                        $mid[$j] = array($id => array($line_member[$i]->user_id));
+                                        $mid[$j] = array($id => $line_member[$i]->user_id);
 
                                     ?>
 
