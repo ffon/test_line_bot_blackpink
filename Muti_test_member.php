70f2144b3da0a425f4193b585de7d16cb5ce0fff
ffon <poty1900@hotmail.com>
2017-08-03 16:22:17 +0700

    Update Muti_test_member.php

--- Muti_test_member.php
+++ Muti_test_member.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="th">
-1212
+1414
 <br>
 <head>
     <title>Push Messages</title>
@@ -107,10 +107,10 @@
                             for ($j=0; $j<$count_id; $j++) {
                                 if ($id[$j]==$line_master[$i]->id) {
                                      $token[$j] = array($id[$j] => $line_master[$i]->access_token);
-                                     $token_en =  json_encode($token);
-                                     $token_de = json_decode($token_en);
+                                     $token_en = urlencode($token);
+                                     $token_de = urldecode($token_en);
                                     ?>
-                                    <input type="hidden" value="<? var_dump($token_de); ?>" name="token[]"/>
+                                    <input type="hidden" value="<? print_r($token_de); ?>" name="token[]"/>
                                     <?php
                                 }
                             }
