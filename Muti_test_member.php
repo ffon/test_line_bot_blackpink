2e0bc7f74f52b63e773e181fbcf6fcb597e85b72
ffon <poty1900@hotmail.com>
2017-08-03 18:14:06 +0700

    Update Muti_test_member.php

--- Muti_test_member.php
+++ Muti_test_member.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="th">
-81
+8
 <br>
 <head>
     <title>Push Messages</title>
@@ -111,7 +111,7 @@
                                      $token_de = json_decode($token_en);
 
                                     ?>
-                                    <input type="hidden" value="<?php print_r($token[$j]); ?>" name="token[]"/>
+                                    <input type="hidden" value="<?php print_r($token_en); ?>" name="token[]"/>
                                     <?php
                                 }
                             }
