fcff3978fae29e866ccb71640bb11231c51b5265
ffon <poty1900@hotmail.com>
2017-08-04 10:52:11 +0700

    ok

--- Muti_test_push_fix.php
+++ Muti_test_push_fix.php
@@ -1,17 +1,27 @@
 <?php    
     
-    
-    $tokens = array('1'=>'4IBuBmV6FywcyJmUK34M8karKQx3d4ferATfOBddUE5lOKYK4hMX5nn6zHmY0xU435cWVjdmcsqIsDjlsWQNO2siGwPWEXtl7Y67lf2v0nEGIAfvuKDtBJZx4JUtwgb+PJWLcRN8TArRFMfNP3ZGKQdB04t89/1O/w1cDnyilFU=','2'=>'7uCUqHp4ZPaCraRiNL+FYnbgH7KSzEME+hIEZvOf0sxRyEuEjQ9O32liOHwmLUFmcFvVkJH2cMox8g/ml2Ulw7YGORdDhgVXJvKZs24dnQqHfRVdzDJv4ZBpSy4ql5bt3COy1hmKkVdRlDo1swsePgdB04t89/1O/w1cDnyilFU=');
-    // $token = $tokens;
-    // $mid = $mids;
-    // $text = '123';
-    
+    echo "0";
+    $tokens = array('1' => 'bnPCz7RNBSZ2h/PbGjAPN1iaK4PIIF0MlLrar0iOFi5kpcjb8qGcQCQZngEAnsS2QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcs7twRoZc+fC59BcRxhwPWlvmr/nXevWi7w1FWg4QoMBwdB04t89/1O/w1cDnyilFU='
+                    '2' => 'S3VhGqoaXc1OFAxRsYPrIpcuqMXf7Zc9/b9fXM8iXf3EEAJAMIXtoZBlcrdScnb86qVYXGI80LOObJe1H9EaoK4ZfSiSHwpUrRgQxlREc/Y7ZKfNYCcmdBkE+GPik3HsrAnlLnjICCQtAZXij9VHzwdB04t89/1O/w1cDnyilFU=');
+                                //oil,ffon
+    $mids = array('1'=>array('Ub5fea2ff169cba24b2179fd33e59e454','U7de80d0a2ceea863e831375badd2eb55'),
+                  '2'=>array('U30e8563887497419674d43526fb4d878','U7de80d0a2ceea863e831375badd2eb55'));//ffon,code
+    $text = '123';
     var_dump($tokens);
+    var_dump($mids);
 
-    foreach($tokens as $key=>$token){
-        echo $key;
+    foreach($tokens as $key_token => $token){
+        echo $key_token;
         echo "<br>";
         echo $token;
+        echo "<br>";
+        foreach($mids as $key_mid => $mid){
+            echo $key_mid;
+            echo "<br>";
+            echo $mid;
+            echo "<br>";
+        }
+
     }
    
     
