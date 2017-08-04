ea962f7b00f1ad6a15b7610ab5b2c20771e1f668
ffon <poty1900@hotmail.com>
2017-08-04 11:34:56 +0700

    ok

--- Muti_test_push_fix.php
+++ Muti_test_push_fix.php
@@ -1,6 +1,7 @@
-<?php    
+<?php
     
-    echo "3";echo "<br>";
+    echo "2";
+    echo "<br>";
     $tokens = array('1' => 'bnPCz7RNBSZ2h/PbGjAPN1iaK4PIIF0MlLrar0iOFi5kpcjb8qGcQCQZngEAnsS2QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcs7twRoZc+fC59BcRxhwPWlvmr/nXevWi7w1FWg4QoMBwdB04t89/1O/w1cDnyilFU=',
                     '2' => 'S3VhGqoaXc1OFAxRsYPrIpcuqMXf7Zc9/b9fXM8iXf3EEAJAMIXtoZBlcrdScnb86qVYXGI80LOObJe1H9EaoK4ZfSiSHwpUrRgQxlREc/Y7ZKfNYCcmdBkE+GPik3HsrAnlLnjICCQtAZXij9VHzwdB04t89/1O/w1cDnyilFU=');
                                 //oil,ffon
@@ -8,79 +9,64 @@
                   '2'=>array('U30e8563887497419674d43526fb4d878','U7de80d0a2ceea863e831375badd2eb55'));//ffon,code
     $text = '123';
 
-    echo "token";echo "<br>";
-    var_dump($tokens);echo "<br>";
+    echo "token";
+    echo "<br>";
+    var_dump($tokens);
+    echo "<br>";
 
-    echo "mids";echo "<br>";
-    var_dump($mids);echo "<br>";
+    echo "mids";
+    echo "<br>";
+    var_dump($mids);
+    echo "<br>";
 
-    echo "text";echo "<br>";
-    var_dump($text);echo "<br>";
+    echo "text";
+    echo "<br>";
+    var_dump($text);
+    echo "<br>";
 
-    foreach($tokens as $key_token => $token){
-        // echo "key token";
-        // echo "<br>";
-        // echo $key_token;
-        // echo "<br>";
-        // echo $token;
-        // echo "<br>";
-        foreach($mids as $key_mid => $mid){
-            // echo "key mid";
-            // echo "<br>";
-            // echo $key_mid;
-            // echo "<br>";
-            // echo $mid;
-            // echo "<br>";
-            foreach($mid as $key_value => $mid_value){
-                // echo $key_value;
-                // echo "<br>";
-                // echo $mid_value;
-                // echo "<br>";
-                if($key_token == $key_mid){
-                    echo "key token";
-                    echo "<br>";
-                    echo $key_token;echo "<br>";
-                    echo "key mid";
-                    echo "<br>";
-                    echo $key_mid;echo "<br>";
-                    echo "key_token == key_mid";
-                    echo "<br>";
-
-                }
-
-            }
-        }
-
-   }
-   
-    
-    
-// foreach(token as $key => $tokens){
-// foreach ($mids as $key => $mid) {
-//     $messages = [
-//     "type" => "text",
-//     "text" => $text
-//     ];
+    foreach ($tokens as $key_token => $token) {
+        foreach ($mids as $key_mid => $mid) {
+            if ($key_token == $key_mid) {
+                echo "key token";
+                echo "<br>";
+                echo $key_token;
+                echo "<br>";
+                echo "key mid";
+                echo "<br>";
+                echo $key_mid;
+                echo "<br>";
+                echo "key_token == key_mid";
+                echo "<br>";
+                foreach ($mid as $key_value => $mid_value) {
+                    $messages = [
+                    "type" => "text",
+                    "text" => $text
+                    ];
  
-//     $post_data = [
-//     "to" => $mid,
-//     "messages" => [$messages]
-//     ];
+                    $post_data = [
+                    "to" => $mid_value,
+                    "messages" => [$messages]
+                    ];
    
         
-//         $header = array(
-//         'Content-Type: application/json',
-//         'Authorization: Bearer ' . $tokens
-//         );
+                    $header = array(
+                    'Content-Type: application/json',
+                    'Authorization: Bearer ' . $token
+                    );
     
-//     $url = 'https://api.line.me/v2/bot/message/push';
-//     $ch = curl_init($url);
-//     curl_setopt($ch, CURLOPT_POST, true);
-//     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-//     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-//     $result = curl_exec($ch);
-//     curl_close($ch);
-// }
-// }
+                    $url = 'https://api.line.me/v2/bot/message/push';
+                    $ch = curl_init($url);
+                    curl_setopt($ch, CURLOPT_POST, true);
+                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+                    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+                    $result = curl_exec($ch);
+                    curl_close($ch);
+                }
+            }
+        }
+    }
+    echo "<br>";
+    echo "result";echo "<br>";
+    var_dump($result);
