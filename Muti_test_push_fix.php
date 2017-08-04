889abbb8224c052611922dcd223896afe8bd4c99
ffon <poty1900@hotmail.com>
2017-08-04 10:59:39 +0700

    ok

--- Muti_test_push_fix.php
+++ Muti_test_push_fix.php
@@ -1,15 +1,15 @@
 <?php    
     
-   
-    $tokens = array('1' => 'bnPCz7RNBSZ2h/PbGjAPN1iaK4PIIF0MlLrar0iOFi5kpcjb8qGcQCQZngEAnsS2QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcs7twRoZc+fC59BcRxhwPWlvmr/nXevWi7w1FWg4QoMBwdB04t89/1O/w1cDnyilFU='
-                    '2' => 'S3VhGqoaXc1OFAxRsYPrIpcuqMXf7Zc9/b9fXM8iXf3EEAJAMIXtoZBlcrdScnb86qVYXGI80LOObJe1H9EaoK4ZfSiSHwpUrRgQxlREc/Y7ZKfNYCcmdBkE+GPik3HsrAnlLnjICCQtAZXij9VHzwdB04t89/1O/w1cDnyilFU=');
+   echo "5555";
+    // $tokens = array('1' => 'bnPCz7RNBSZ2h/PbGjAPN1iaK4PIIF0MlLrar0iOFi5kpcjb8qGcQCQZngEAnsS2QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcs7twRoZc+fC59BcRxhwPWlvmr/nXevWi7w1FWg4QoMBwdB04t89/1O/w1cDnyilFU='
+                    // '2' => 'S3VhGqoaXc1OFAxRsYPrIpcuqMXf7Zc9/b9fXM8iXf3EEAJAMIXtoZBlcrdScnb86qVYXGI80LOObJe1H9EaoK4ZfSiSHwpUrRgQxlREc/Y7ZKfNYCcmdBkE+GPik3HsrAnlLnjICCQtAZXij9VHzwdB04t89/1O/w1cDnyilFU=');
                                 //oil,ffon
     // $mids = array('1'=>array('Ub5fea2ff169cba24b2179fd33e59e454','U7de80d0a2ceea863e831375badd2eb55'),
     //               '2'=>array('U30e8563887497419674d43526fb4d878','U7de80d0a2ceea863e831375badd2eb55'));//ffon,code
-    $text = '123';
-    var_dump($tokens);
+    // $text = '123';
+    // var_dump($tokens);
    // var_dump($mids);
-    var_dump($text);
+    // var_dump($text);
 
     // foreach($tokens as $key_token => $token){
     //     echo $key_token;
