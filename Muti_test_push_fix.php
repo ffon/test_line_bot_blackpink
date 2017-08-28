54f0aeceb1994c3a115b97f9717e961109486166
ffon <poty1900@hotmail.com>
2017-08-28 16:27:28 +0700

    Update Muti_test_push_fix.php

--- Muti_test_push_fix.php
+++ Muti_test_push_fix.php
@@ -26,49 +26,49 @@
     var_dump($text);
     echo "<br>";
 
-    foreach ($tokens as $key_token => $token) {
-        foreach ($mids as $key_mid => $mid) {
-            if ($key_token == $key_mid) {
-                // echo "key token";
-                // echo "<br>";
-                // echo $key_token;
-                // echo "<br>";
-                // echo "key mid";
-                // echo "<br>";
-                // echo $key_mid;
-                // echo "<br>";
-                // echo "key_token == key_mid";
-                // echo "<br>";
-                foreach ($mid as $key_value => $mid_value) {
-                    $messages = [
-                    "type" => "text",
-                    "text" => $text
-                    ];
+//     foreach ($tokens as $key_token => $token) {
+//         foreach ($mids as $key_mid => $mid) {
+//             if ($key_token == $key_mid) {
+//                 // echo "key token";
+//                 // echo "<br>";
+//                 // echo $key_token;
+//                 // echo "<br>";
+//                 // echo "key mid";
+//                 // echo "<br>";
+//                 // echo $key_mid;
+//                 // echo "<br>";
+//                 // echo "key_token == key_mid";
+//                 // echo "<br>";
+//                 foreach ($mid as $key_value => $mid_value) {
+//                     $messages = [
+//                     "type" => "text",
+//                     "text" => $text
+//                     ];
  
-                    $post_data = [
-                    "to" => $mid_value,
-                    "messages" => [$messages]
-                    ];
+//                     $post_data = [
+//                     "to" => $mid_value,
+//                     "messages" => [$messages]
+//                     ];
    
         
-                    $header = array(
-                    'Content-Type: application/json',
-                    'Authorization: Bearer ' . $token
-                    );
+//                     $header = array(
+//                     'Content-Type: application/json',
+//                     'Authorization: Bearer ' . $token
+//                     );
     
-                    $url = 'https://api.line.me/v2/bot/message/push';
-                    $ch = curl_init($url);
-                    curl_setopt($ch, CURLOPT_POST, true);
-                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-                    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-                    $result = curl_exec($ch);
-                    curl_close($ch);
-                }
-            }
-        }
-    }
-    echo "<br>";
-    echo "result";echo "<br>";
-    var_dump($result);
+//                     $url = 'https://api.line.me/v2/bot/message/push';
+//                     $ch = curl_init($url);
+//                     curl_setopt($ch, CURLOPT_POST, true);
+//                     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+//                     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//                     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+//                     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+//                     $result = curl_exec($ch);
+//                     curl_close($ch);
+//                 }
+//             }
+//         }
+//     }
+//     echo "<br>";
+//     echo "result";echo "<br>";
+//     var_dump($result);
